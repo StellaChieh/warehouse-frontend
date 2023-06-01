37f984997fff079ccca416861051893aecd06c54
Hui-Chun Chieh <stella.chieh@gmail.com>
2023-06-01 17:56:03 -0400

    finish form local validation

--- src/ZoneForm.js
+++ src/ZoneForm.js
@@ -3,12 +3,59 @@ import { Button, Col, Form, Row } from 'react-bootstrap';
 
 export default function Zone ({zoneNumber}) {
 
+    function onlyLettersAndNumbers(str) {
+        return /^[A-Za-z0-9]*$/.test(str);
+    }
+
+    function validateShelfName(shelfNames) {
+        let emptyNameCount = 0
+        const existingNames = new Set();
+        const errorMsgList = new Array();
+        for (let i = 0; i< 10; i++) {
+            const shelfName = shelfNames[i]
+            // check if the shelf name is not empty
+            if (shelfName.length == 0) {
+                emptyNameCount++;
+                continue;
+            }
+            // check if name is unique in this zone
+            if (existingNames.has(shelfName)) {
+                errorMsgList.push(shelfName + " is not a unique name in the Zone.")
+            } else {
+                existingNames.add(shelfName)
+            }
+            
+            // check if name only contains English letters and numbers
+            if (!onlyLettersAndNumbers(shelfName)) {
+                errorMsgList.push(shelfName + " is not a valid shelf name.")
+            }
+        }
+        
+        if (emptyNameCount == 10) {
+            errorMsgList.push("There is no shelf name defined.")
+        }
+
+        if (errorMsgList.length == 0) {
+            return { validateStatus: true }
+        } else {
+            return {
+                validateStatus: false,
+                msg: errorMsgList
+            }
+        }
+    }
+
     function submitHandler(e) {
         e.preventDefault()
         const formData = new FormData(e.target)
         const formDataObj = Object.fromEntries(formData.entries())
-        console.log(formDataObj)
-        console.log("zone: ", zoneNumber);
+        const { validateStatus, msg} = validateShelfName(formDataObj)
+        if (validateStatus) {
+            alert('Y')
+        } else {
+            alert(msg)
+        }
+
     }
 
     let columNumber = 0
