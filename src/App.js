998fdd75bbd070cad25ee48766509e24b6e703f9
Hui-Chun Chieh <stella.chieh@gmail.com>
2023-06-02 09:29:02 -0400

    using prettier formatter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import './App.css';
-import Zone from './ZoneForm';
-import { Container, Accordion } from 'react-bootstrap';
-
+import "./App.css";
+import Zone from "./ZoneForm";
+import { Container, Accordion } from "react-bootstrap";
 
 function App() {
-  const zoneIndex = Array.from({ length:12 }, (value, index) => index);
+  const zoneIndex = Array.from({ length: 12 }, (value, index) => index);
   const zones = zoneIndex.map((val, index) => {
-    const zoneNumber = index+1
+    const zoneNumber = index + 1;
     return (
       <Accordion.Item eventKey={zoneNumber} key={zoneNumber}>
         <Accordion.Header>Zone {zoneNumber}</Accordion.Header>
         <Accordion.Body>
-          <Zone zoneNumber={zoneNumber}/>      
+          <Zone zoneNumber={zoneNumber} />
         </Accordion.Body>
       </Accordion.Item>
     );
   });
-  
+
   return (
     <Container className="App">
       <h1>Define a Warehouse</h1>
-      <p>1. Every shift name should be unique across whole warehouse.<br/>
-      2. The length of a shift name can not be longer than 20 characters.<br/>
-      3. Only English letters and numbers are allowed in shift names.</p>
+      <p>
+        1. Every shift name should be unique across whole warehouse.
+        <br />
+        2. The length of a shift name can not be longer than 20 characters.
+        <br />
+        3. Only English letters and numbers are allowed in shift names.
+      </p>
       <Accordion defaultActiveKey="1" alwaysOpen>
         {zones}
-      </Accordion>  
+      </Accordion>
     </Container>
   );
 }
-
 
 export default App;
--- src/ZoneForm.js
+++ src/ZoneForm.js
@@ -1,112 +1,111 @@
-import { Button, Col, Form, Row } from 'react-bootstrap';
+import { Button, Col, Form, Row } from "react-bootstrap";
 
+export default function Zone({ zoneNumber }) {
+  function onlyLettersAndNumbers(str) {
+    return /^[A-Za-z0-9]*$/.test(str);
+  }
 
-export default function Zone ({zoneNumber}) {
+  function validateShelfName(shelfNames) {
+    let emptyNameCount = 0;
+    const existingNames = new Set();
+    const errorMsgList = new Array();
+    for (let i = 0; i < 10; i++) {
+      const shelfName = shelfNames[i];
+      // check if the shelf name is not empty
+      if (shelfName.length == 0) {
+        emptyNameCount++;
+        continue;
+      }
+      // check if the length of shalf name is smaller than 20
+      if (shelfName.length > 20) {
+        errorMsgList.push(shelfName + " is too long (maximum 20 characters).");
+      }
+      // check if name is unique in this zone
+      if (existingNames.has(shelfName)) {
+        errorMsgList.push(shelfName + " is not a unique name in the Zone.");
+      } else {
+        existingNames.add(shelfName);
+      }
 
-    function onlyLettersAndNumbers(str) {
-        return /^[A-Za-z0-9]*$/.test(str);
+      // check if name only contains English letters and numbers
+      if (!onlyLettersAndNumbers(shelfName)) {
+        errorMsgList.push(shelfName + " is not a valid shelf name.");
+      }
     }
 
-    function validateShelfName(shelfNames) {
-        let emptyNameCount = 0
-        const existingNames = new Set();
-        const errorMsgList = new Array();
-        for (let i = 0; i< 10; i++) {
-            const shelfName = shelfNames[i]
-            // check if the shelf name is not empty
-            if (shelfName.length == 0) {
-                emptyNameCount++;
-                continue;
-            }
-            // check if the length of shalf name is smaller than 20
-            if (shelfName.length > 20) {
-                errorMsgList.push(shelfName + " is too long (maximum 20 characters).")
-            }
-            // check if name is unique in this zone
-            if (existingNames.has(shelfName)) {
-                errorMsgList.push(shelfName + " is not a unique name in the Zone.")
-            } else {
-                existingNames.add(shelfName)
-            }
-            
-            // check if name only contains English letters and numbers
-            if (!onlyLettersAndNumbers(shelfName)) {
-                errorMsgList.push(shelfName + " is not a valid shelf name.")
-            }
-        }
-        
-        if (emptyNameCount == 10) {
-            errorMsgList.push("There is no shelf name defined.")
-        }
-
-        if (errorMsgList.length == 0) {
-            return { validateStatus: true }
-        } else {
-            return {
-                validateStatus: false,
-                msg: errorMsgList
-            }
-        }
+    if (emptyNameCount == 10) {
+      errorMsgList.push("There is no shelf name defined.");
     }
 
-    function submitHandler(e) {
-        e.preventDefault()
-        const formData = new FormData(e.target)
-        const formDataObj = Object.fromEntries(formData.entries())
-        const { validateStatus, msg} = validateShelfName(formDataObj)
-        if (validateStatus) {
-            alert('Y')
-        } else {
-            alert(msg)
-        }
-
+    if (errorMsgList.length == 0) {
+      return { validateStatus: true };
+    } else {
+      return {
+        validateStatus: false,
+        msg: errorMsgList,
+      };
     }
+  }
 
-    let columNumber = 0
-    return (
-        <Form onSubmit={submitHandler}>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-            </Row>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-            </Row>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-            </Row>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-            </Row>
-            <Row className="mb-3">
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-                <Col>
-                    <Form.Control placeholder="Input Shelf Name" name={columNumber++}/>
-                </Col>
-            </Row>
-            <Button variant="primary" type="submit" size="sm"> Submit </Button>
-        </Form>
-        
-    );
-};
+  function submitHandler(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObj = Object.fromEntries(formData.entries());
+    const { validateStatus, msg } = validateShelfName(formDataObj);
+    if (validateStatus) {
+      alert("Y");
+    } else {
+      alert(msg);
+    }
+  }
+
+  let columNumber = 0;
+  return (
+    <Form onSubmit={submitHandler}>
+      <Row className="mb-3">
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+        <Col>
+          <Form.Control placeholder="Input Shelf Name" name={columNumber++} />
+        </Col>
+      </Row>
+      <Button variant="primary" type="submit" size="sm">
+        {" "}
+        Submit{" "}
+      </Button>
+    </Form>
+  );
+}
